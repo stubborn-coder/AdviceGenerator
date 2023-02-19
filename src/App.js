58e51ebcc4aa8bcd831bff67726f49f373a8087d
Muhammad Bilal <mbbilal0612@gmail.com>
2023-02-19 17:21:24 +0500

    Added more css

--- src/App.js
+++ src/App.js
@@ -7,22 +7,37 @@ const App = () => {
 
   const [advice2,setAdvice] = useState("");
 
-  const fetchAdvice = axios.get('https://api.adviceslip.com/advice')
+  let fetchAdvice = () =>{ axios.get('https://api.adviceslip.com/advice')
   .then((object)=>{
     object = object.data.slip;
     let { id,advice } = object;
     setAdvice(advice);
 
+    let button = document.getElementsByClassName("button");
+    if(!button[0].className.includes("disabled")){
+      button.disabled = true;
+      button[0].className = button[0].className+" disabled"
+      setTimeout(()=>{
+        button[0].className = button[0].className.replace("disabled","");
+        button.disabled=false;
+      },"120000");
+    }
+    
+
   }).catch((error)=>{
     console.log(error)
     console.log("Data not loaded from API")
   });
+ }
+  fetchAdvice();
 
   return (
     <div className='app'>
       <div className='box'>
-        <h1>Advice Generator</h1>
         <h2>{advice2}</h2>
+        <button className='button' onClick={fetchAdvice}>
+          <span>Give me new advice</span>
+        </button>
       </div>
     </div>
   )
